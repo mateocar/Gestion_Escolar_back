30ee9e00a60ac6ce03dde10ca365962113aa4f05
eldelahoz <andelahoz13@gmail.com>
2024-09-25 22:39:40 -0500

    ADD: Modelo usuario

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.models):
+    ROLES = (
+        ('student', 'Estudiante'),
+        ('teacher', 'Profesor'),
+        ('administrator', 'Administrador')
+    )
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    date_birth = models.DateField()
+    address = models.AutoField(100)
+    rol = models.CharField(max_length=15, choices=ROLES)
+    username = models.CharField(100)
+    password = models.CharField(50)
