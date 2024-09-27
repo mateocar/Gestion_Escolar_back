3218d191db2a56bc6fda4c4a886719603a1ed87f
eldelahoz <andelahoz13@gmail.com>
2024-09-27 06:59:34 -0500

    ADD: Modelo de roles

---
+++ app/roles/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Role(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
